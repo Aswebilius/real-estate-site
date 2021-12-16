505f7521987a615af163d20637d25645061d99b9
Aswebilius <81036333+Aswebilius@users.noreply.github.com>
2021-12-16 01:13:37 -0500

    implementing header, footer, and navbar

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Flex, Box, Text, Button } from '@chakra-ui/react'
 import { baseUrl, fetchApi } from '../utils/fetchApi'
+import Property from '../components/Property'
 
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl}) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
     <Image src={imageUrl} width={500} height={300} alt='banner'/>
     <Box p='5'>
-      <Text color='gray.300' fontSize='sm' fontWeight='medium'>{purpose}</Text>
+      <Text color='red.500' fontSize='sm' fontWeight='medium' fontFamily='cursive'>{purpose}</Text>
       <Text fontSize='3xl' fontWeight='bold'>{title1} <br /> {title2}</Text>
       <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1} <br /> {desc2}</Text>
       <Button fontSize='xl'>
@@ -34,7 +35,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'/>
 
       <Flex flexWrap='wrap'>
-
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
       </Flex>
 
       <Banner purpose={'For Rent'}
@@ -45,6 +46,10 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
       buttonText='Explore Renting'
       linkName='/search?purpose=for-rent'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'/>
+
+<Flex flexWrap='wrap'>
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
       </Box>
     
   )
